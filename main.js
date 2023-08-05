42d6a66dcd08177c394e8070b4566b6b250362be
amShuri <106229595+amShuri@users.noreply.github.com>
2023-08-05 05:00:35 -0300

    Add game functionality and basic UI
    Added the core game functionality. The game is now fully playable with a basic UI made using Bootstrap and a project-specific CSS file that includes some key styles that wouldn't be possible with Bootstrap alone.

---
+++ main.js
@@ -0,0 +1,80 @@
+const computerSequence = [];
+const userSequence = [];
+const TURN_DURATION_MS = 1000;
+const HIGHLIGHT_DURATION_MS = 500;
+
+document.querySelector('#start-button').onclick = function () {
+  if (computerSequence.length > 0) {
+    computerSequence.splice(0);
+  }
+  playComputerTurn();
+};
+
+document.querySelector('#game-board').onclick = function (e) {
+  if (e.target.tagName !== 'BUTTON') return;
+  const $userTile = e.target;
+  userSequence.push($userTile);
+  highlightTile($userTile, HIGHLIGHT_DURATION_MS);
+
+  const $computerTile = computerSequence[userSequence.length - 1];
+  if ($computerTile !== $userTile) {
+    stopGame();
+    return;
+  }
+
+  if (computerSequence.length === userSequence.length) {
+    disableUserInput();
+    setTimeout(() => {
+      playComputerTurn();
+    }, TURN_DURATION_MS);
+  }
+};
+
+function getRandomTile() {
+  const $gameTiles = document.querySelectorAll('.tile');
+  const randomNumber = Math.floor(Math.random() * $gameTiles.length);
+  return $gameTiles[randomNumber];
+}
+
+function playComputerTurn() {
+  if (userSequence.length > 0) {
+    userSequence.splice(0);
+  }
+  disableUserInput();
+
+  const $computerTile = getRandomTile();
+  computerSequence.push($computerTile);
+
+  for (let i = 0; i < computerSequence.length; i += 1) {
+    const currentTile = computerSequence[i];
+    const highlightDelay = (i + 1) * TURN_DURATION_MS;
+    setTimeout(() => {
+      highlightTile(currentTile, HIGHLIGHT_DURATION_MS);
+    }, highlightDelay);
+  }
+
+  const computerTurnDuration = (computerSequence.length + 1) * TURN_DURATION_MS;
+  setTimeout(() => {
+    enableUserInput();
+  }, computerTurnDuration);
+}
+
+function highlightTile(tile, duration) {
+  tile.classList.add('highlighted');
+  setTimeout(() => {
+    tile.classList.remove('highlighted');
+  }, duration);
+}
+
+function disableUserInput() {
+  document.querySelectorAll('.tile').forEach((btn) => (btn.disabled = true));
+}
+function enableUserInput() {
+  document.querySelectorAll('.tile').forEach((btn) => (btn.disabled = false));
+}
+
+function stopGame() {
+  alert('Game Over');
+  disableUserInput();
+  document.querySelector('#start-button').textContent = 'Play Again';
+}
